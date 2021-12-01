a846023fda409ca7449cd22827abcf7da9637ed5
mawir157 <jamesmthompson2@gmail.com>
2021-12-01 15:23:32 +0000

    [01] :snowflake: :christmas_tree:

--- Haskell/Day01.hs
+++ Haskell/Day01.hs
@@ -1,12 +1,12 @@
 import AdventHelper
 
 diff_off :: [Integer] -> Int -> [(Integer, Integer)]
-diff_off x o = filter (\(l,r) -> l > r) (zip (drop o x) x)
+diff_off x o = filter (\(l,r) -> l < r) $ zip x $ drop o x
 
 main = do
   putStrLn "Day 1"
   f <- readFile "../input/input01.txt"
-  let ss = map(read) $ lines f :: [Integer]
+  let ss = map read $ lines f :: [Integer]
 
-  printSoln 1 (length $ diff_off ss 1)
-  printSoln 2 (length $ diff_off ss 3)
+  printSoln 1 $ length $ diff_off ss 1
+  printSoln 2 $ length $ diff_off ss 3
